d47ba79c30d3330faff3222343fb256ee3306eeb
Yuki Okuno <y.okuno.btm@gmail.com>
2024-10-30 22:13:46 +0900

    User.nicknameに文字数バリデーションを追加

--- src/spec/factories/users.rb
+++ src/spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname { 'テスト太郎' }
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
