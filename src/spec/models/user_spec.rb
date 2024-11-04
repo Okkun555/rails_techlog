c7f1350c47a51decf240861e7634c8541e6b8b66
Okkun555 <y.okuno.btm@gmail.com>
2024-11-04 20:48:21 +0900

    Deviseライブラリを導入

---
+++ src/spec/models/user_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:nickname) { 'テスト太郎' }
+  let(:email) { 'test@example.com' }
+  let(:password) { '12345678' }
+  let(:user) { User.new(nickname: nickname, email: email, password: password, password_confirmation: password) }
+
+  describe '.first' do
+    before do
+      create(:user, nickname: nickname, email: email)
+    end
+
+    subject { described_class.first }
+
+    it '事前に作成した通りのUserを返す' do
+      expect(subject.nickname).to eq('テスト太郎')
+      expect(subject.email).to eq('test@example.com')
+    end
+  end
+
+  describe 'validation' do
+    describe 'nickname属性' do
+      describe '文字数制限の検証' do
+        context 'nicknameが20文字以下の場合' do
+          let(:nickname) { 'a' * 20 }
+
+          it 'Userオブジェクトは有効である' do
+            expect(user.valid?).to be(true)
+          end
+        end
+
+        context 'nickanmeが20文字を超える場合' do
+          let(:nickname) { 'a' * 21 }
+
+          it 'Userオブジェクトは無効である' do
+            expect(user.valid?).to be(false)
+            expect(user.errors[:nickname]).to include('is too long (maximum is 20 characters)')
+          end
+        end
+      end
+
+      describe 'nickname存在性の検証' do
+        context 'nicknameが空欄の場合' do
+          let(:nickname) { '' }
+
+          it 'Userオブジェクトは無効である' do
+            expect(user.valid?).to be(false)
+            expect(user.errors[:nickname]).to include("can't be blank")
+          end
+        end
+      end
+    end
+  end
+end
